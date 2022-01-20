386491009284acf8ace71be103dc8709cf97fb66
jurgest <ballajurgest@gmail.com>
2022-01-20 12:54:13 +0100

    search users form,get and clear

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,25 +1,14 @@
-import React, {useEffect, useState} from 'react';
+import React, { useContext} from 'react';
 import Spinner from '../layout/assets/Spinner';
 import UserItem from './UserItem';
-
+import GithubContext from '../../context/github/GithubContext';
 const UserResults = () => {
-    const [users, setUsers] = useState([])
-    const [loading, setLoading] = useState(true)
+    const {users, loading} = useContext(GithubContext);
 
-    useEffect(()=> {
-        fetchUsers();
-    },[])
+    // useEffect(()=> {
+    //     fetchUsers();
+    // },[])
 
-    const fetchUsers =async ()=> {
-        const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`,
-        {headers: { 
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-           }})
-        const data = await response.json()
-        setUsers(data)
-        setLoading(false)
-    }
     if(!loading) {
         return <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
             {users.map((user)=> (
--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,26 +1,45 @@
-import { createContext, useState } from "react";
+import { createContext, useReducer } from "react";
+import githubReducer from "./GithubReducer";
 
 const GithubContext = createContext()
 
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
 
 export const GithubProvider = ({children}) => {
-    const [users, setUsers] = useState([])
-    const [loading, setLoading] = useState(true)
+    const initialState = {
+        users: [],
+        loading: false
+    }
 
-    const fetchUsers =async ()=> {
-        const response = await fetch(`${GITHUB_URL}/users`,
+    const [state, dispatch] = useReducer(githubReducer, initialState)
+
+    const searchUsers =async (text)=> {
+        setLoading()
+        const params = new URLSearchParams({
+            q: text
+        })
+        const response = await fetch(`${GITHUB_URL}/search/users?${params}`,
         {headers: { 
             'Content-Type': 'application/json',
             'Accept': 'application/json'
            }})
-        const data = await response.json()
-        setUsers(data)
-        setLoading(false)
+        const {items} = await response.json()
+        dispatch({
+            type: 'GET_USERS',
+            payload: items
+        })
     }
+    //set loading
+    const setLoading = () => dispatch({type: 'SET_LOADING'})
+
+    //clear results
+    const clearUsers = () => dispatch({type: 'CLEAR_USERS'})
+
     return <GithubContext.Provider value={{
-        users ,
-        loading
+        users: state.users,
+        loading: state.loading,
+        searchUsers,
+        clearUsers
     }}>
         {children}
     </GithubContext.Provider>
